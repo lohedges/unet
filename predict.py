cc707ad93b37290bb7e2d6b99dab19b65711d1d2
Matt Williams <matt@milliams.com>
2019-06-21 14:03:36 +0100

    Adjust gradient in predictions

--- predict.py
+++ predict.py
@@ -2,9 +2,23 @@ from pathlib import Path
 
 import mrcfile
 import numpy as np
+import sklearn.linear_model
 import skimage.io
 import skimage.transform
 import tensorflow as tf
+
+
+def adjust_gradient(image):
+    m, n = image.shape
+    R, C = np.mgrid[:m, :n]
+    out = np.column_stack((C.ravel(), R.ravel(), image.ravel()))
+
+    reg = sklearn.linear_model.LinearRegression().fit(out[:, 0:2], out[:, 2])
+
+    plane = np.fromfunction(lambda x, y: y * reg.coef_[0] + x * reg.coef_[1] + reg.intercept_, image.shape)
+
+    return image - plane
+
 
 my_model = tf.keras.models.load_model('my_model.hdf5')
 
@@ -17,7 +31,7 @@ with mrcfile.open(file, permissive=True) as mrc:
 
 d = skimage.transform.resize(d, (256, 256))
 
-#d_cv = image.adjust_gradient(d_cv)
+d = adjust_gradient(d)
 
 #d_cv = image.normalise_float(d_cv)
 d_min = d.min()
