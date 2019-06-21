2d9f8dff17e3c526962fbad45a9eb13ccef115cc
Matt Williams <matt@milliams.com>
2019-06-21 14:37:35 +0100

    Delete old augmentation settings

--- main.py
+++ main.py
@@ -18,15 +18,6 @@ data_gen_args = dict(
     fill_mode='reflect',  # reflect?
     #preprocessing_function=foo,  # something to do the warping
 )
-# data_gen_args = dict(
-#     rotation_range=0.2,
-#     width_shift_range=0.05,
-#     height_shift_range=0.05,
-#     shear_range=0.05,
-#     zoom_range=0.05,
-#     horizontal_flip=True,
-#     fill_mode='nearest'
-# )
 train, validate, test = data.load_data(
     "/home/matt/proof_example_data/unet_foo/input_png",
     "/home/matt/proof_example_data/unet_foo/masks",
