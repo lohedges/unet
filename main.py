eb425ed20206f8c970405b192e7e8faf9ac180b8
Matt Williams <matt@milliams.com>
2020-01-29 09:57:51 +0000

    Add GPU printing

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import tensorflow as tf
 from model import unet
 from data import trainGenerator, testGenerator, saveResult
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        #for gpu in gpus:
+        #    tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
 
 data_gen_args = dict(
     rotation_range=15,  # degrees
