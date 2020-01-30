bd9d6c4bcffe9e054a7ecc9c945ad5ed602244b0
Matt Williams <matt@milliams.com>
2020-01-30 11:24:00 +0000

    Use PEP8 naming

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ import skimage.io as io
 import skimage.transform as trans
 
 
-def adjustData(img, mask):
+def adjust_data(img, mask):
     img = img / 255
     mask = mask / 255
     mask[mask > 0.5] = 1
@@ -14,9 +14,9 @@ def adjustData(img, mask):
     return img, mask
 
 
-def trainGenerator(batch_size, train_path, image_folder, mask_folder, aug_dict, image_color_mode="grayscale",
-                   mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
-                   flag_multi_class=False, num_class=2, save_to_dir=None, target_size=(256, 256), seed=1):
+def train_generator(batch_size, train_path, image_folder, mask_folder, aug_dict, image_color_mode="grayscale",
+                    mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
+                    save_to_dir=None, target_size=(256, 256), seed=1):
     """
     can generate image and mask at the same time
     use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
@@ -71,12 +71,12 @@ def trainGenerator(batch_size, train_path, image_folder, mask_folder, aug_dict, 
         seed=seed,
         subset="validation",
     )
-    train_gen = (adjustData(img, mask) for img, mask in zip(image_generator, mask_generator))
-    valid_gen = (adjustData(img, mask) for img, mask in zip(val_image_generator, val_mask_generator))
+    train_gen = (adjust_data(img, mask) for img, mask in zip(image_generator, mask_generator))
+    valid_gen = (adjust_data(img, mask) for img, mask in zip(val_image_generator, val_mask_generator))
     return train_gen, valid_gen
 
 
-def testGenerator(test_path, target_size=(256, 256), flag_multi_class=False, as_gray=True):
+def test_generator(test_path, target_size=(256, 256), as_gray=True):
     fn = "FoilHole_24681291_Data_24671848_24671849_20181025_0148-78831.png"
     img = io.imread(os.path.join(test_path, fn), as_gray=as_gray)
     img = img / 255
@@ -86,7 +86,7 @@ def testGenerator(test_path, target_size=(256, 256), flag_multi_class=False, as_
     yield img
 
 
-def saveResult(save_path, npyfile):
+def save_result(save_path, npyfile):
     for i, item in enumerate(npyfile):
         img = item[:, :, 0]
         io.imsave(os.path.join(save_path, "%d_predict.png" % i), img)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import tensorflow as tf
 
 from model import unet
-from data import trainGenerator, testGenerator, saveResult
+from data import train_generator, test_generator, save_result
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
@@ -57,7 +57,7 @@ data_gen_args = dict(
     #preprocessing_function=foo,  # something to do the warping
     validation_split=0.1,  # Fraction of data to use for validation
 )
-train, validate = trainGenerator(batch_size, 'data/filament/train', 'image', 'label', data_gen_args, save_to_dir=None)
+train, validate = train_generator(batch_size, 'data/filament/train', 'image', 'label', data_gen_args, save_to_dir=None)
 
 model = unet(learning_rate=learning_rate)
 model_checkpoint = tf.keras.callbacks.ModelCheckpoint(str(output_dir/'unet_filament.hdf5'), monitor='val_loss', verbose=1, save_best_only=True)
@@ -65,6 +65,6 @@ es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', verbose=1, patience=10
 tensorboard = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1, write_images=True)
 model.fit(train, steps_per_epoch=steps_per_epoch, epochs=epochs, callbacks=[model_checkpoint, es, tensorboard], validation_data=validate, validation_steps=1)
 
-testGene = testGenerator("data/filament/test")
+testGene = test_generator("data/filament/test")
 results = model.predict(testGene, verbose=1)
-saveResult(output_dir, results)
+save_result(output_dir, results)
