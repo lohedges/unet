74a4fc8064e9216b10107d3d479a2269d4689335
Matt Williams <matt@milliams.com>
2020-01-30 11:20:04 +0000

    Remove unused code

--- data.py
+++ data.py
@@ -1,47 +1,16 @@
-from __future__ import print_function
-import numpy as np 
+import numpy as np
 import os
-import glob
 
 import tensorflow as tf
 import skimage.io as io
 import skimage.transform as trans
 
-Sky = [128, 128, 128]
-Building = [128, 0, 0]
-Pole = [192, 192, 128]
-Road = [128, 64, 128]
-Pavement = [60, 40, 222]
-Tree = [128, 128, 0]
-SignSymbol = [192, 128, 128]
-Fence = [64, 64, 128]
-Car = [64, 0, 128]
-Pedestrian = [64, 64, 0]
-Bicyclist = [0, 128, 192]
-Unlabelled = [0, 0, 0]
 
-COLOR_DICT = np.array([Sky, Building, Pole, Road, Pavement,
-                       Tree, SignSymbol, Fence, Car, Pedestrian, Bicyclist, Unlabelled])
-
-
-def adjustData(img, mask, flag_multi_class, num_class):
-    if flag_multi_class:
-        img = img / 255
-        mask = mask[:, :, :, 0] if(len(mask.shape) == 4) else mask[:, :, 0]
-        new_mask = np.zeros(mask.shape + (num_class,))
-        for i in range(num_class):
-            #for one pixel in the image, find the class in mask and convert it into one-hot vector
-            #index = np.where(mask == i)
-            #index_mask = (index[0],index[1],index[2],np.zeros(len(index[0]),dtype = np.int64) + i) if (len(mask.shape) == 4) else (index[0],index[1],np.zeros(len(index[0]),dtype = np.int64) + i)
-            #new_mask[index_mask] = 1
-            new_mask[mask == i, i] = 1
-        new_mask = np.reshape(new_mask, (new_mask.shape[0], new_mask.shape[1]*new_mask.shape[2], new_mask.shape[3])) if flag_multi_class else np.reshape(new_mask, (new_mask.shape[0]*new_mask.shape[1], new_mask.shape[2]))
-        mask = new_mask
-    elif np.max(img) > 1:
-        img = img / 255
-        mask = mask / 255
-        mask[mask > 0.5] = 1
-        mask[mask <= 0.5] = 0
+def adjustData(img, mask):
+    img = img / 255
+    mask = mask / 255
+    mask[mask > 0.5] = 1
+    mask[mask <= 0.5] = 0
     return img, mask
 
 
@@ -102,8 +71,8 @@ def trainGenerator(batch_size, train_path, image_folder, mask_folder, aug_dict, 
         seed=seed,
         subset="validation",
     )
-    train_gen = (adjustData(img, mask, flag_multi_class, num_class) for img, mask in zip(image_generator, mask_generator))
-    valid_gen = (adjustData(img, mask, flag_multi_class, num_class) for img, mask in zip(val_image_generator, val_mask_generator))
+    train_gen = (adjustData(img, mask) for img, mask in zip(image_generator, mask_generator))
+    valid_gen = (adjustData(img, mask) for img, mask in zip(val_image_generator, val_mask_generator))
     return train_gen, valid_gen
 
 
@@ -112,37 +81,12 @@ def testGenerator(test_path, target_size=(256, 256), flag_multi_class=False, as_
     img = io.imread(os.path.join(test_path, fn), as_gray=as_gray)
     img = img / 255
     img = trans.resize(img, target_size)
-    img = np.reshape(img, img.shape+(1,)) if (not flag_multi_class) else img
+    img = np.reshape(img, img.shape+(1,))
     img = np.reshape(img, (1,)+img.shape)
     yield img
 
 
-def geneTrainNpy(image_path, mask_path, flag_multi_class=False, num_class=2, image_prefix="image", mask_prefix="mask", image_as_gray=True, mask_as_gray=True):
-    image_name_arr = glob.glob(os.path.join(image_path, "%s*.png" % image_prefix))
-    image_arr = []
-    mask_arr = []
-    for index, item in enumerate(image_name_arr):
-        img = io.imread(item, as_gray=image_as_gray)
-        img = np.reshape(img, img.shape + (1,)) if image_as_gray else img
-        mask = io.imread(item.replace(image_path, mask_path).replace(image_prefix, mask_prefix), as_gray=mask_as_gray)
-        mask = np.reshape(mask, mask.shape + (1,)) if mask_as_gray else mask
-        img, mask = adjustData(img, mask, flag_multi_class,num_class)
-        image_arr.append(img)
-        mask_arr.append(mask)
-    image_arr = np.array(image_arr)
-    mask_arr = np.array(mask_arr)
-    return image_arr, mask_arr
-
-
-def labelVisualize(num_class, color_dict, img):
-    img = img[:, :, 0] if len(img.shape) == 3 else img
-    img_out = np.zeros(img.shape + (3,))
-    for i in range(num_class):
-        img_out[img == i, :] = color_dict[i]
-    return img_out / 255
-
-
-def saveResult(save_path, npyfile, flag_multi_class=False, num_class=2):
+def saveResult(save_path, npyfile):
     for i, item in enumerate(npyfile):
-        img = labelVisualize(num_class, COLOR_DICT, item) if flag_multi_class else item[:, :, 0]
+        img = item[:, :, 0]
         io.imsave(os.path.join(save_path, "%d_predict.png" % i), img)
